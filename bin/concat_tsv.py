fba98653de6a2e5d14369a68dbf60775e2173975
Sabrina Krakau <sabrina.krakau.qbic@gmail.com>
2023-02-02 15:59:21 +0100

    Run Black

--- bin/concat_tsv.py
+++ bin/concat_tsv.py
@@ -29,9 +29,7 @@ import math
 
 def parse_args(args=None):
     """Parses the command line arguments specified by the user."""
-    parser = argparse.ArgumentParser(
-        description="Concatenate TSV files."
-    )
+    parser = argparse.ArgumentParser(description="Concatenate TSV files.")
 
     # INPUT FILES
     parser.add_argument("-i", "--input", help="Path to input files.", type=str, required=True, nargs="+")
@@ -62,23 +60,24 @@ def main(args=None):
         with pd.read_csv(filename, sep="\t", chunksize=args.chunk_size) as reader:
             for j, tsv_chunk in enumerate(reader):
                 print(" Chunk: ", j, flush=True)
-                if (i == 0 and j == 0):
+                if i == 0 and j == 0:
                     first_header = tsv_chunk.columns
                     print("Header: ", first_header.tolist(), flush=True)
-                    tsv_chunk.to_csv(
-                        args.output, mode="w", sep="\t", index=False, header=True
-                    )
+                    tsv_chunk.to_csv(args.output, mode="w", sep="\t", index=False, header=True)
                 else:
-                    if (j == 0):
+                    if j == 0:
                         # Check if header of subsequent input files match header of first input file
                         # (column order must be the same)
-                        if (tsv_chunk.columns.tolist() != first_header.tolist()):
-                            print("ERROR - header of input file", filename, "does not match the header of the first input file!", file=sys.stderr)
+                        if tsv_chunk.columns.tolist() != first_header.tolist():
+                            print(
+                                "ERROR - header of input file",
+                                filename,
+                                "does not match the header of the first input file!",
+                                file=sys.stderr,
+                            )
                             sys.exit(1)
 
-                    tsv_chunk.to_csv(
-                        args.output, mode="a", sep="\t", index=False, header=False
-                    )
+                    tsv_chunk.to_csv(args.output, mode="a", sep="\t", index=False, header=False)
 
 
 if __name__ == "__main__":
