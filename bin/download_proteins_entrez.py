4a249d27738ed17e3d49e48ac3cf88b99dd47ec4
Till Englert <till.englert96@gmail.com>
2023-02-16 14:16:28 +0100

    Change logic to check file format before assigning abundance 1

--- bin/download_proteins_entrez.py
+++ bin/download_proteins_entrez.py
@@ -141,12 +141,12 @@ def main(args=None):
                     file=args.microbiomes_entities,
                     flush=True,
                     )
-            elif "taxon_id" in row.keys() and not "abundance" in row.keys():
-                taxIds.append(row["taxon_id"])
-                print(row["taxon_id"], microbiomeId, 1, sep="\t", file=args.microbiomes_entities, flush=True)
             elif not "taxon_id" in row.keys() or len(row.keys())>=2:
                 sys.exit(f"The format of the input file '{taxid_input.name}' is invalid!" +
-                         "It needs to be a csv file containing taxon_id,abundance or just taxon_id")
+                         "It needs to be a csv file containing taxon_id,abundance or just taxon_id.")
+            elif "taxon_id" in row.keys():
+                taxIds.append(row["taxon_id"])
+                print(row["taxon_id"], microbiomeId, 1, sep="\t", file=args.microbiomes_entities, flush=True)
 
     taxIds = list(set(taxIds))
     print("Processing the following taxonmy IDs:")
