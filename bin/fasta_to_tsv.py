2869fd8b33d8ebebe9d53b222b080cd5c407c08f
Till Englert <till.englert96@gmail.com>
2023-03-29 13:19:12 +0200

    Change filehandler in fasta to tsv parsing

--- bin/fasta_to_tsv.py
+++ bin/fasta_to_tsv.py
@@ -17,18 +17,20 @@
 
 import argparse
 import sys
+import gzip
 from Bio import SeqIO
 
 parser = argparse.ArgumentParser("Converts FASTA to TSV")
-parser.add_argument("-i", "--input", type=argparse.FileType("r"), help="Input FASTA file", default=sys.stdin)
+parser.add_argument("-i", "--input", help="Input FASTA file", default=sys.stdin)
 parser.add_argument("-o", "--output", type=argparse.FileType("o"), help="Output TSV file", default=sys.stdout)
 parser.add_argument(
     "-ra", "--remove-asterisk", action="store_true", help="Remove trailing asterisks produced by prodigal"
 )
 args = parser.parse_args()
 
-for record in SeqIO.parse(args.input, "fasta"):
-    if args.remove_asterisk and record.seq[-1] == "*":
-        print(f"{record.id}\t{record.seq[:-1]}")
-    else:
-        print(f"{record.id}\t{record.seq}")
+with gzip.open(args.input, "rt") as handle:
+    for record in SeqIO.parse(handle, "fasta"):
+        if args.remove_asterisk and record.seq[-1] == "*":
+            print(f"{record.id}\t{record.seq[:-1]}")
+        else:
+            print(f"{record.id}\t{record.seq}")
