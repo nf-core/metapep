69937177a8f3ade28c5f49ba70a89845889d34de
Till Englert <till.englert96@gmail.com>
2023-08-01 10:01:18 +0200

    Bugfix peptide Lengths and include log instead of tsv

--- bin/unify_model_lengths.py
+++ bin/unify_model_lengths.py
@@ -45,17 +45,13 @@ def parse_args():
     parser.add_argument(
         "-plh", "--peptide_max_len", help="Maximum length of peptide used for prediction", type=int, default=13
     )
-
     parser.add_argument(
-        "-o",
-        "--output",
+        "-log_s",
+        "--output_log_suffix",
         required=True,
-        help=(
-            "Path to the output file. The output file will be a "
-            "plain text tsv file with a column containing the allele name, a column containing the length and a column containing the corresponding allele models."
-        ),
-        type=argparse.FileType("w"),
-        default="unified_allele_models.tsv",
+        help="Suffix for the output log file.",
+        type=str,
+        default="_unify_peptide_lengths",
     )
     return parser.parse_args()
 
@@ -78,26 +74,37 @@ def check_model_availability(model_name, prediction_method):
 
 def main():
     args = parse_args()
+    log_str = ""
+
+    log_str += "### Unify Peptide Lengths ###\n\n"
 
     samplesheet = pd.read_csv(args.input)
 
     # Retrieve unique list of alleles
     alleles_s = {allele for allele_list in samplesheet["alleles"] for allele in allele_list.split(" ")}
+    log_str += f"Found the following alleles: {', '.join(alleles_s)}\n\n"
     # Parse alleles to epytope convention
     predictor = EpitopePredictorFactory(args.method)
     alleles = [allele_from_string(allele) for allele in alleles_s]
     conv_alleles = predictor.convert_alleles(alleles)
     # Check if a model is available at given lengths
+    input_lengths = [i for i in range(args.peptide_min_len, args.peptide_max_len + 1)]
+    log_str += f"Check if models are available at given lengths: {', '.join(map(str, input_lengths))}\n\n"
     allele_availability = []
     for conv_allele, allele_s in zip(conv_alleles, alleles_s):
-        for pep_len in range(args.peptide_min_len, args.peptide_max_len + 1):
+        for pep_len in input_lengths:
             model_name = f"{conv_allele}_{pep_len}"
-            availibility = check_model_availability(model_name, args.method)
-            allele_availability.append([allele_s, pep_len, model_name, availibility])
+            availability = check_model_availability(model_name, args.method)
+            if availability:
+                log_str += f"Found model for allele {allele_s} with length {pep_len}\n"
+            else:
+                log_str += f"No model found for allele {allele_s} with length {pep_len}\n"
+            allele_availability.append([allele_s, pep_len, model_name, availability])
 
     allele_availability = pd.DataFrame(
         allele_availability, columns=["Allele", "Peptide_Length", "Allele_Model", "Availability"]
     )
+
 
     # Drop all non available models
     allele_availability = allele_availability[allele_availability["Availability"]]
@@ -107,10 +114,25 @@ def main():
     len_sets = [set(allele_models["Peptide_Length"]) for allele, allele_models in allele_availability.groupby("Allele")]
     len_intersect = set.intersection(*len_sets)
 
+    log_str += "\nReducing the used peptide lengths to the common denominator\n"
+    log_str += f"Following lengths are used for the epitope prediction on the alleles {', '.join(alleles_s)}: {', '.join(map(str, len_intersect))}\n"
+    log_str += "All other peptide lengths are omitted from further analysis."
     # Remove all non fitting lengths
     allele_availability = allele_availability[allele_availability["Peptide_Length"].isin(len_intersect)]
 
-    allele_availability.to_csv(args.output, sep="\t", index=False)
+    if len_intersect == set(input_lengths):
+        log_prefix = "SUCCEEDED"
+    elif len(len_intersect) == 0:
+        log_prefix = "ERROR"
+    else:
+        log_prefix = "WARNING"
+    log_fname = log_prefix + args.output_log_suffix + ".log"
+
+    with open(log_fname, "w") as log:
+        log.write(log_str)
+
+    # Output the unified lengths to the stdout, so it can be used as output for the process
+    print(",".join(str(l) for l in len_intersect))
 
 
 if __name__ == "__main__":
